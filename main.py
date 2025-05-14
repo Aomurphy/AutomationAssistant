d1f6ad9a6a9fe698a6a1058024b4f562571a2c48
April Murphy <aomurphy2002@gmail.com>
2025-05-13 23:38:24 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
+import time
 
 # Launch Chrome
-
 chrome_driver = webdriver.Chrome()
+chrome_driver.get('https://gmail.com/')
+time.sleep(2)
 
-chrome_driver.get('https://bstackdemo.com/')
-
+#Print title in chrome
 print("Title in Chrome:", chrome_driver.title)
 
+chrome_driver.implicitly_wait(25) # seconds
+
+
+#Quit Chrome
 chrome_driver.quit()
 
-# Launch Firefox
+'''
+Launch Firefox
 
 firefox_driver = webdriver.Firefox()
 
@@ -18,4 +24,4 @@ firefox_driver.get("https://bstackdemo.com/")
 
 print("Title in Firefox:", firefox_driver.title)
 
-firefox_driver.quit()
+firefox_driver.quit()'''
