1f1a2cb8e01f0bb474aa0b76ba07793af48955a2
April Murphy <aomurphy2002@gmail.com>
2025-05-14 16:40:55 -0400

    Accessed chrome and selected elements.

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 # Launch Chrome
-chrome_driver = webdriver.Chrome()
-chrome_driver.get('https://gmail.com/')
-time.sleep(2)
+url = 'https://gmail.com/'
+driver = webdriver.Chrome()
+username = 'bunnyboo1717'
+
+driver.get(url)
+time.sleep(1)
+driver.implicitly_wait(25) # seconds
+driver.find_element(By.ID,'identifierId').send_keys(username)
+time.sleep(1)
+driver.find_element(By.ID,'identifierNext').click()
+time.sleep(5)
 
 #Print title in chrome
-print("Title in Chrome:", chrome_driver.title)
+print("Title in Chrome:", driver.title)
 
-chrome_driver.implicitly_wait(25) # seconds
+#driver.implicitly_wait(25) # seconds
 
 
 #Quit Chrome
-chrome_driver.quit()
+#chrome_driver.quit()
 
 '''
 Launch Firefox
